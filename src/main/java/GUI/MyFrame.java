5c3ea7669488a3e7e208fd7b8d4b6ce69b40cb75
Alexx <32265866+Alexxfromgit@users.noreply.github.com>
2023-05-02 01:25:36 +0300

    Created layout

---
+++ src/main/java/GUI/MyFrame.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Alexxfromgit
+ */
+public class MyFrame extends JFrame implements ActionListener {
+
+    /*
+    MyFrame(){
+        this.SetDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.SetLayout(new FlowLayout());
+
+        JButton button = new JButton("Select File");
+        button.addActionListener(this);
+        this.add(button);
+
+        this.pack();
+        this.SetVisible(true);
+    }
+*/
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
