0b4556eab65ceef21709715b9c1601b09438148e
Alexander Rubtsov <d5enemy@gmail.com>
2024-08-03 20:57:04 +0300

    PDFMetaViewer::Add simple base tests

--- src/test/java/testCases/PdfConvertLogicTests.java
+++ src/test/java/testCases/PdfConvertLogicTests.java
@@ -1,19 +1,20 @@
 package testCases;
 
+import extractor.PDFExtractor;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import repository.DocumentRecord;
 import utils.TestFailureLog;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static utilities.PdfMetaViewerConstants.*;
 
 @Listeners({
         TestFailureLog.class
 })
-public class GeneralConfigurationTests {
+public class PdfConvertLogicTests {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws IOException {
@@ -21,13 +22,8 @@ public class GeneralConfigurationTests {
     }
 
     @Test(groups = "smoke")
-    public void testGetAppTitle() {
-        assertThat(APP_MAIN_TITLE).isEqualToIgnoringCase("Pdf Meta Viewer");
-    }
-
-    @Test(groups = "smoke")
-    public void testGetAppDefaultDimensions() {
-        assertThat(WIDTH_APP).isEqualTo(800);
-        assertThat(HEIGHT_APP).isEqualTo(500);
+    public void testPdfExtractor() {
+        assertThat(new PDFExtractor().getDocumentRecordFromImportedPDF("src/test/resources/Amazon-S3-Tutorial.pdf")
+                .getContent()).isNotEmpty();
     }
 }
